e53ddd71f2126c964c5eee6e86fac3f35c03b7e9
Vladimir Ivanov <infositeforvova@gmail.com>
2023-03-20 18:58:20 +0500

    Lesson 4

---
+++ Les.4/dom.js
@@ -0,0 +1,10 @@
+// DOM - объектная модель документа
+// BOM - браузерная модель документа
+// window - работа с DOM и BOM
+// Взаимодействие с DOM через document
+// document.body, document.head
+
+// DOM
+
+let divs = document.getElementsByTagName('div');
+console.log(divs);
