68fd1e769ee81a4a2aee6474649957426b1fa5c1
Vladimir Ivanov <infositeforvova@gmail.com>
2023-03-22 12:24:57 +0500

    Lesson 4 + dz

--- Les.4/dz.js
+++ Les.4/dz.js
@@ -37,17 +37,47 @@ function reverseArray (array) {
     return array.sort((a,b) => b - a );
 }
 // 3. Написать функцию, которая принимает три аргумента: произвольный массив и два числа, первое из которых означает начальный номер элемента в массиве, второе - конечный номер.
-// Функция должна вернуть новый массив, состоящий из элементов первой коллекции согласно аргументам (с - по): getNewArray(['a', 'b', 'c', 'd', 'e', 'f'], 2, 4) = ["c", "d", "e"]
+// Функция должна вернуть новый массив, состоящий из элементов первой коллекции согласно аргументам (с - по): 
+// getNewArray(['a', 'b', 'c', 'd', 'e', 'f'], 2, 4) = ["c", "d", "e"]
 // Исходный массив не менять. Циклы не использовать.
 function getNewArray (array, startNumber, lastNumber) {
+    let newArr = [].concat(array);
+    newArr.splice(lastNumber+1);
+    newArr.splice(0,startNumber);
+    return newArr;
+}
+
+function getNewArray1 (array, startNumber, lastNumber) {
+    let newArr = array.slice(startNumber,lastNumber+1);
+    return newArr;
 }
 // 4. Удвоить все элементы массива, не используя циклы
 // [‘one’, 2, ‘three’, 4] → [‘one’, 2, ‘three’, 4, ‘one’, 2, ‘three’, 4]
+function doubleArray1 (arr) {
+    let newArr = [].concat(arr).concat(arr);
+  
+    return newArr;
+}
 // 5. Удалить из [1,2,3,4,5] второй и третий элементы (3,4)
+let arr = [1,2,3,4,5];
+arr.splice(2, 2)
 // 6. Удалить из [1,2,3,4,5] второй и третий элементы (3,4) и на их место вставить ‘three’, ‘four’
+let arr1 = [1,2,3,4,5];
+arr1.splice(2, 2, 'three', 'four');
 // 7. Вставить в произвольный массив после третьего элемента любое значение, например:  ['I', 'am', 'an', 'array'] → ["I", "am", "an", "awesome", "array"]
-// 8. Отсортируйте массив массивов так, чтобы вначале располагались наименьшие массивы (размер массива определяется его длиной):  [ [14, 45], [18], ['a', 'c', 'd'] ] → [ [18], [14, 45], ['a', 'c', 'd'] ]
+let arr2 = ['I', 'am', 'an', 'array'];
+arr2.splice(3, 0, "awesome");
+// 8. Отсортируйте массив массивов так, чтобы вначале располагались наименьшие массивы (размер массива определяется его длиной):  
+//[ [14, 45], [18], ['a', 'c', 'd'] ] → [ [18], [14, 45], ['a', 'c', 'd'] ]
+let arr3 = [ 
+    [14, 45], [18],
+     ['a', 'c', 'd'] 
+]
+arr3.sort((a,b) => a.length-b.length)
 // 9.	Создать копию произвольного массива (slice, concat)
+let arr4 = [1,2,3,4,44,4,4];
+let arr5 = [].concat(arr4);
+let arr6 = arr4.slice();
 // 10.	Есть массив объектов:
 // [
 //   {cpu: 'intel', info: {cores:2, сache: 3}},
@@ -57,7 +87,14 @@ function getNewArray (array, startNumber, lastNumber) {
 //   {cpu: 'amd', info: {cores:4, сache: 2}}
 // ]
 // Отсортировать их по возрастающему количеству ядер (cores).
-
+let arr7 =  [
+    {cpu: 'intel', info: {cores:2, сache: 3}},
+    {cpu: 'intel', info: {cores:4, сache: 4}},
+    {cpu: 'amd', info: {cores:1, сache: 1}},
+    {cpu: 'intel', info: {cores:3, сache: 2}},
+    {cpu: 'amd', info: {cores:4, сache: 2}}
+    ]
+arr7.sort((a,b) => a.info.cores - b.info.cores);
 // 11.	Создать функцию, которая будет принимать массив продуктов и две цены. 
 // Функция должна вернуть все продукты, цена которых находится в указанном диапазоне, и отсортировать от дешевых к дорогим:   const products = [
 //   {title: 'prod1', price: 5.2}, {title: 'prod2', price: 0.18},
@@ -66,4 +103,13 @@ function getNewArray (array, startNumber, lastNumber) {
 //   {title: 'prod7', price: 19}, {title: 'prod8', price: 63}
 // ];
 // filterCollection(products, 15, 30) → [{...price: 15}, {...price: 18.9}, {...price: 19}, {...price: 25}]
-
+function filterCollection (array, price1, price2) {
+    let newArr = []
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].price >= price1 && array[i].price <= price2 ) {
+            newArr[i] = array[i]
+        }
+    }
+    newArr.sort((a,b) => a.price - b.price)
+    return newArr;
+}
