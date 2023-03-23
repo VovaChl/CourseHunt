cbea731634cfc81e8cb3045ceb51e6de29d386a0
Vladimir Ivanov <infositeforvova@gmail.com>
2023-03-23 17:50:15 +0500

    Lesson 5 + dz

---
+++ Les.5/dz.js
@@ -0,0 +1,47 @@
+/* <div>
+  <article>
+    <p>Lorem ipsum dolor sit amet, odio omnesque ius cu, quo ex atqui antiopam. At detracto menandri eos. Duo in causae viderer, graeci <a href="#">reprehendunt</a> has in. Decore <mark>nemore</mark> philosophia te pro, nobis legere causae ex mei, odio putant mentitum ea ius. Vix nostro deserunt explicari eu.</p>
+  </article>
+</div>
+<ul>
+  <li><a href="#">Link1</a></li>
+  <li><a href="#">Link2</a></li>
+  <li><a href="#">Link3</a></li>
+  <li><a href="#">Link4</a></li>
+</ul><span></span>
+<a href="#">Some link</a> */
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     console.clear();
+//    ваш код поместить тут
+    
+//   });
+
+
+// 1. Не используя innerHTML, добавить в список несколько li с классом ‘new-item’ и текстом ‘item’ + номер 
+//  <ul>    
+//     <li><a href="#">Link1</a></li>
+//     <li class=”new-item”>item 4</li>
+//     <li class=”new-item”>item 5</li>
+// </ul>
+
+// 2. Создать три элемента strong и добавить их в конец ссылок, которые находятся внутри списка ul (в каждую ссылку один - strong)
+
+// 3. В начало документа (в начало body) добавить картинку img с атрибутами src и alt (текст придумайте сами). В src добавьте реальный url к картинке
+
+// 4. Найти на странице элемент mark, добавить в конец содержимого текст “green” и на элемент установить класс green
+
+// 5. Отсортировать li внутри списка в обратном порядке (по тексту внутри)
+
+// 6. Сделать задачу отсюда
+// Библиотека для работы с html элементами
+// Добавить методы в nodes конструктор:
+// - метод append(html) добавляет в конец содержимого узла html;
+// - метод prepend(html) добавляет в начало содержимого;
+// - метод before(html) вставляет html перед узлом;
+// - метод after(html) вставляет html после узла;
+// - метод html([value]) должен вернуть html-содержимое узла (innerHTML), если ни один аргумент не передан, и заменить содержимое на value, если функция вызвана с аргументом;
+// - метод getElement содержит переданный узел (метод должен вернуть узел DOM, переданный при инициализации конструктора).
+
+// На проверку - только файл nodes.js. 
+// В дальнейшем будем его использовать и для других задач.
