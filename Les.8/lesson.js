3cf7af91dde747b1f05a19e54346cdbebea2c458
Vladimir Ivanov <infositeforvova@gmail.com>
2023-04-04 00:30:52 +0500

    Lesson 8

---
+++ Les.8/lesson.js
@@ -0,0 +1,62 @@
+// // Date
+// let date = new Date();
+// let methods;
+
+// date.setMinutes(date.getMinutes + 10);
+
+// date = date.toLocaleString('ru-RU', {
+//     year: '2-digit',
+//     month: 'short',
+//     weekday: 'long',
+// });
+
+// // // setInterval
+// // setInterval(() => {
+// //     console.log(Date.now());
+// // }, 500); //бесконечно каждые 0.5 сек будет показывтаь дату
+
+
+// // let timer = setInterval(() => {
+// //     console.log(Date.now());
+// // }, 500); //также бесконечно, но можно оставить
+
+// // clearInterval(timer); //останавливает исполнение интервала timer
+
+// // forEach для массивов
+// let buttons = document.querySelectorAll('.timer__button');
+
+// // let array = [1, 2, 'one'];
+
+// // array.forEach((value,index,array) => {
+// //     console.log(value,index,array);
+// // });
+// function clickEvent (e) {
+
+// }
+// // обработчик на каждую кнопку
+// //buttons.forEach(btn => btn.addEventListener('click', clickEvent));
+
+// buttons.forEach(() => {});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
