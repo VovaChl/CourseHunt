588b9abd878245f56f0d1745ec7d9212c7f289c5
Vladimir Ivanov <infositeforvova@gmail.com>
2023-04-12 23:07:10 +0500

    Lesson 10

---
+++ Les.10/plugin.js
@@ -0,0 +1,16 @@
+// AJAX
+// FETCH
+// Socket
+
+
+// url, route
+// request, req - запрос к серверу
+// response, res - ответ от сервера
+// methods:
+// get, GET - получение данных от сервера
+// post, POST - передача данных серверу
+// put, PUT - обновляение данных на сервере
+// delete, DELETE - удаление данных на сервере
+// Headers - заголовки (доп.информация)
+
+// jsonplaceholder.typicode.com
