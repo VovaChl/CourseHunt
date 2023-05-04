103353efe7e9c7ba1d1861c062ff692b8d9b2f2b
Vladimir Ivanov <infositeforvova@gmail.com>
2023-05-04 21:40:35 +0500

    Lesson 14 + dz

---
+++ Les.14/ajax.js
@@ -0,0 +1,48 @@
+// settings = {
+//    method: 'GET',
+//    url: ''https:...',
+//    data: 'some info',h
+//    headers: {},
+//    timeout: 3000,
+//    success: function on success,
+//    error: function on error,
+// }
+
+const ajax = (function () {
+
+    function send(settings) {
+
+        const xhr = new XMLHttpRequest();
+
+        xhr.addEventListener('error', function (ev) {
+            settings.error({
+                errorText: xhr.responseText,
+                code: xhr.status,
+            });
+        });
+
+        xhr.addEventListener('load', function (ev) {
+            settings.success(xhr.responseText);
+        });
+
+        xhr.addEventListener('timeout', function (ev) {
+            // some action on timeout;
+        })
+
+        xhr.open(settings.method, settings.url);
+
+        xhr.timeout = settings.timeout || 3000;
+
+        if (settings.headers) {
+            for (let headerName in settings.headers) {
+                xhr.setRequestHeader(headerName, settings.headers[headerName]);
+            }
+        }
+        xhr.send(settings.data);
+
+    }
+
+    return {
+        send: send,
+    }
+})();
